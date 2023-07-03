45bc51a4ddd6ba2b99f3a351ce582196cea8ff0c
Giovani <giovanibulian@hotmail.com>
2023-07-03 14:47:33 -0400

    + path compression to the WQU

--- union_find/WeightedQuickUnion.py
+++ union_find/WeightedQuickUnion.py
@@ -8,11 +8,11 @@ class WeightedQuickUnionSize(QuickUnion):
         super().__init__(n)
         self.size = [1 for _ in range(n)]
     
-    def union(self, element1: int, element2: int) -> int:
-        self.validate_element_index(element1, element2)
+    def union(self, node1: int, node2: int) -> int:
+        self.validate_node_index(node1, node2)
         
-        root1 = self.find(element1)
-        root2 = self.find(element2)
+        root1 = self.find(node1)
+        root2 = self.find(node2)
         size_root1 = self.size[root1]
         size_root2 = self.size[root2]
         
@@ -39,16 +39,17 @@ class WeightedQuickUnionHeight(QuickUnion):
         super().__init__(n)
         self.height = [0 for _ in range(n)]
     
-    def union(self, element1: int, element2: int) -> int:
-        self.validate_element_index(element1, element2)
+    def union(self, node1: int, node2: int) -> int:
+        self.validate_node_index(node1, node2)
         
-        root1 = self.find(element1)
-        root2 = self.find(element2)
-        height_root1 = self.height[root1]
-        height_root2 = self.height[root2]
+        root1 = self.find(node1)
+        root2 = self.find(node2)
         
         if root1 == root2:
             return self.sets_count
+        
+        height_root1 = self.height[root1]
+        height_root2 = self.height[root2]
         
         if height_root1 == height_root2:
             self.parent[root2] = root1
@@ -61,4 +62,105 @@ class WeightedQuickUnionHeight(QuickUnion):
         self.sets_count -= 1
             
         return self.sets_count
+    
         
+class WeightedQuickUnionHeightPathCompression(WeightedQuickUnionHeight):
+    '''
+    Implementation of weighted quick union by (root)height with
+    path compression
+    
+    e.g.:
+                0  1  2  3  4  5  6  7
+    parent = [0, 0, 1, 2, 3, 2, 5, 5]
+    
+    find(7): 
+        1)  root == 0
+            
+        2.1)    touched_node == 7
+        
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 2, 3, 2, 5, 0]
+                    
+        2.2)    touched_node == 5
+        
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 2, 3, 0, 5, 0]
+                    
+        2.3)    touched_node == 2
+        
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 0, 2, 3, 0, 5, 0]
+                    
+        2.4)    touched_node == 0
+                return root
+    '''
+    def find(self, node):
+        self.validate_node_index(node)
+        
+        root = node
+        
+        # 1) find the root
+        while root != self.parent[root]:
+            root = self.parent[root]
+        
+        # 2) set each touched node (from `node` to `root`)
+        # to point to `root`
+        touched_node = node
+        while touched_node != root:
+            self.parent[touched_node], touched_node = \
+                root, self.parent[touched_node]
+        
+        return root
+
+    
+class WeightedQuickUnionHeightPathCompression2(WeightedQuickUnionHeight):
+    '''
+    Implementation of weighted quick union by (root)height with
+    path compression
+    
+    e.g.:
+                  0  1  2  3  4  5  6  7
+        parent = [0, 0, 1, 2, 3, 4, 5, 6]
+        
+        find(7): 
+            1)  root = 7
+                grandparent = 5
+                
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 2, 3, 4, 5, 5]
+                
+            2)  root = 5
+                grandparent = 3
+                
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 2, 3, 3, 5, 5]
+                
+            3)  root = 3
+                grandparent = 1
+                
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 1, 3, 3, 5, 5]
+                
+            4)  root = 1
+                grandparent = 0
+                
+                node      0  1  2  3  4  5  6  7
+                parent = [0, 0, 1, 1, 3, 3, 5, 5]
+                
+            5)  root = 0
+                root == parent[root]
+                return root
+    '''
+    def find(self, node):
+        self.validate_node_index(node)
+        
+        root = node
+        
+        while root != self.parent[root]:
+            # set the node parent to it's grandparent
+            grandparent = self.parent[self.parent[root]]
+            self.parent[root] = grandparent
+            root = self.parent[root]
+    
+        return root
+    
--- union_find/tests.py
+++ union_find/tests.py
@@ -1,32 +1,36 @@
 import unittest
 from QuickUnion import QuickUnion
 from QuickFind import QuickFind
-from WeightedQuickUnion import WeightedQuickUnionSize, WeightedQuickUnionHeight
+from WeightedQuickUnion import WeightedQuickUnionSize, WeightedQuickUnionHeight, WeightedQuickUnionHeightPathCompression
 
 class TestQuickUnion(unittest.TestCase):
+    def setUp(self) -> None:
+        self.new_union_find = QuickUnion
+        
+    
     def test_valid_object_creation(self):
-        n = 3
-        uf = QuickUnion(3)
-        self.assertEqual(uf.sets_count, n)
+        self.n = 3
+        uf = self.new_union_find(self.n)
+        self.assertEqual(uf.sets_count, self.n)
 
     def test_invalid_object_creation(self):
         negative_n = -1
         zero_n = 0
         with self.assertRaises(ValueError):
-            QuickUnion(negative_n)
+            self.new_union_find(negative_n)
         with self.assertRaises(ValueError):
-            QuickUnion(zero_n)
+            self.new_union_find(zero_n)
 
     def test_find(self):
         n = 3
-        uf = QuickUnion(n)
+        uf = self.new_union_find(n)
 
-        for element in range(3):
-            self.assertEqual(uf.find(element), element)
+        for node in range(3):
+            self.assertEqual(uf.find(node), node)
 
     def test_find_argument_not_in_range(self):
         n = 3
-        uf = QuickUnion(n)
+        uf = self.new_union_find(n)
 
         with self.assertRaises(IndexError):
             uf.find(-n)
@@ -35,7 +39,7 @@ class TestQuickUnion(unittest.TestCase):
 
     def test_union(self):
         n = 3
-        uf = QuickUnion(n)
+        uf = self.new_union_find(n)
 
         # store return value to test
         sets_count = uf.union(0, 1)
@@ -49,7 +53,7 @@ class TestQuickUnion(unittest.TestCase):
 
     def test_union_argument_not_in_range(self):
         n = 3
-        uf = QuickUnion(n)
+        uf = self.new_union_find(n)
 
         with self.assertRaises(IndexError):
             uf.union(0, -n)
@@ -58,23 +62,27 @@ class TestQuickUnion(unittest.TestCase):
 
     def test_relation_transitive(self):
         n = 3
-        uf = QuickUnion(n)
+        uf = self.new_union_find(n)
 
         union_operations = [
             (0, 1),
             (0, 2),
         ]
 
-        for element1, element2 in union_operations:
-            uf.union(element1, element2)
+        for node1, node2 in union_operations:
+            uf.union(node1, node2)
 
         self.assertTrue(uf.are_connected(1, 2))
 
 
 class TestQuickFind(TestQuickUnion):
+    def setUp(self) -> None:
+        self.new_union_find = QuickFind
+        
+    
     def test_union(self):
         n = 3
-        qf = QuickFind(n)
+        qf = self.new_union_find(n)
 
         # store return value to test
         sets_count = qf.union(0, 1)
@@ -93,16 +101,19 @@ class TestQuickFind(TestQuickUnion):
         
 
 class TestWeightedQuickUnionSize(TestQuickUnion):
+    def setUp(self) -> None:
+        self.new_union_find = WeightedQuickUnionSize
+    
     def test_size_init(self):
         n = 3
-        uf = WeightedQuickUnionSize(n)
+        uf = self.new_union_find(n)
         
         for size in uf.size:
             self.assertEqual(size, 1)
         
     def test_size(self):
         n = 5
-        uf = WeightedQuickUnionSize(n)
+        uf = self.new_union_find(n)
         uf.union(0, 1)
         self.assertEqual(uf.size[0], 2)
         uf.union(2, 0)
@@ -114,22 +125,61 @@ class TestWeightedQuickUnionSize(TestQuickUnion):
 
         
 class TestWeightedQuickUnionHeight(TestQuickUnion):
+    def setUp(self) -> None:
+        self.new_union_find = WeightedQuickUnionHeight
+    
     def test_height_init(self):
         n = 3
-        uf = WeightedQuickUnionHeight(n)
+        uf = self.new_union_find(n)
         
         for height in uf.height:
             self.assertEqual(height, 0)
         
     def test_same_height(self):
         n = 2
-        uf = WeightedQuickUnionHeight(n)
+        uf = self.new_union_find(n)
         uf.union(0, 1)
         self.assertEqual(uf.height[0], 1)
     
     def test_different_height(self):
         n = 9
-        uf = WeightedQuickUnionHeight(n)
+        uf = self.new_union_find(n)
+        uf.union(0, 1)
+        self.assertEqual(uf.height[uf.find(0)], 1)
+        uf.union(1, 2)
+        self.assertEqual(uf.height[uf.find(0)], 1)
+        uf.union(3, 4)
+        uf.union(4, 0)
+        self.assertEqual(uf.height[uf.find(0)], 2)
+        uf.union(5, 6)
+        uf.union(7, 8)
+        uf.union(8, 5)
+        self.assertEqual(uf.height[uf.find(5)], 2)
+        uf.union(5, 0)
+        self.assertEqual(uf.height[uf.find(5)], 3)
+        self.assertEqual(max(uf.height), 3)
+
+
+class TestWeightedQuickUnionHeightPathCompression(TestQuickUnion):
+    def setUp(self) -> None:
+        self.new_union_find = WeightedQuickUnionHeightPathCompression
+    
+    def test_height_init(self):
+        n = 3
+        uf = self.new_union_find(n)
+        
+        for height in uf.height:
+            self.assertEqual(height, 0)
+        
+    def test_same_height(self):
+        n = 2
+        uf = self.new_union_find(n)
+        uf.union(0, 1)
+        self.assertEqual(uf.height[0], 1)
+    
+    def test_different_height(self):
+        n = 9
+        uf = self.new_union_find(n)
         uf.union(0, 1)
         self.assertEqual(uf.height[uf.find(0)], 1)
         uf.union(1, 2)

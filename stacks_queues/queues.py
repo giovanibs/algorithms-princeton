45325abc72544355fa370a1820b2fd66c4c22682
Giovani <giovanibulian@hotmail.com>
2023-07-05 00:15:58 -0400

    black and whatnot

--- stacks_queues/queues.py
+++ stacks_queues/queues.py
@@ -1,20 +1,20 @@
 class Queue:
-    '''
+    """
     FIFO queue implementation using Python list
-    '''
-    
+    """
+
     def __init__(self):
         self._queue = []
-        
+
     def len(self):
         return len(self._queue)
-        
+
     def is_empty(self):
         return self.len() == 0
-    
+
     def enqueue(self, item):
         self._queue.insert(0, item)
-        
+
     def dequeue(self):
         if self.is_empty():
             return None
@@ -22,47 +22,47 @@ class Queue:
 
 
 class LinkedListQueue:
-    '''
+    """
     FIFO queue implementation using a linked list data structure
-    '''
-    
+    """
+
     def __init__(self):
         self._first = None
-        self._last  = None
+        self._last = None
         self._len = 0
-        
+
     def len(self):
         return self._len
-        
+
     def is_empty(self):
         return self.len() == 0
-    
+
     def enqueue(self, item):
         if self.is_empty():
             self._first = self._Node(item)
-            self._last  = self._first
+            self._last = self._first
         else:
             # former last will point to the new item
             self._last.next = self._Node(item)
             # the new item is the last
-            self._last  = self._last.next
-            
+            self._last = self._last.next
+
         self._len += 1
-        
+
     def dequeue(self):
         if self.is_empty():
             return None
-        
+
         # retrieve the first item
         first_out = self._first.item
-        
+
         # update queue
         self._first = self._first.next
         self._len -= 1
-        
+
         return first_out
-    
-    class _Node():
+
+    class _Node:
         def __init__(self, item):
             self.item = item
             self.next = None
--- stacks_queues/tests_queues.py
+++ stacks_queues/tests_queues.py
@@ -1,51 +1,54 @@
-from queues import Queue, LinkedListQueue
+from queues import Queue, LinkedListQueue, Deque
 import unittest
+from tests_stacks import LinkedListStackTest
+
 
 class QueueTests(unittest.TestCase):
     def setUp(self):
-        self.q = Queue()
-    
+        self.queue = Queue()
+
     def test_empty_queue(self):
-        self.assertTrue(self.q.is_empty())
-        self.assertEqual(self.q.len(), 0)
-        self.assertIsNone(self.q.dequeue())
+        self.assertTrue(self.queue.is_empty())
+        self.assertEqual(self.queue.len(), 0)
+        self.assertIsNone(self.queue.dequeue())
 
     def test_enqueue_dequeue(self):
-        self.q.enqueue("A")
-        self.q.enqueue("B")
-        self.q.enqueue("C")
-        self.assertEqual(self.q.len(), 3)
-        self.assertEqual(self.q.dequeue(), "A")
-        self.assertEqual(self.q.dequeue(), "B")
-        self.assertEqual(self.q.len(), 1)
-        self.q.enqueue("D")
-        self.assertEqual(self.q.dequeue(), "C")
-        self.assertEqual(self.q.dequeue(), "D")
-        self.assertTrue(self.q.is_empty())
+        self.queue.enqueue("A")
+        self.queue.enqueue("B")
+        self.queue.enqueue("C")
+        self.assertEqual(self.queue.len(), 3)
+        self.assertEqual(self.queue.dequeue(), "A")
+        self.assertEqual(self.queue.dequeue(), "B")
+        self.assertEqual(self.queue.len(), 1)
+        self.queue.enqueue("D")
+        self.assertEqual(self.queue.dequeue(), "C")
+        self.assertEqual(self.queue.dequeue(), "D")
+        self.assertTrue(self.queue.is_empty())
 
     def test_enqueue_dequeue_different_types(self):
-        self.q.enqueue(1)
-        self.q.enqueue("hello")
-        self.q.enqueue([1, 2, 3])
-        self.assertEqual(self.q.len(), 3)
-        self.assertEqual(self.q.dequeue(), 1)
-        self.assertEqual(self.q.dequeue(), "hello")
-        self.assertEqual(self.q.dequeue(), [1, 2, 3])
+        self.queue.enqueue(1)
+        self.queue.enqueue("hello")
+        self.queue.enqueue([1, 2, 3])
+        self.assertEqual(self.queue.len(), 3)
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertEqual(self.queue.dequeue(), "hello")
+        self.assertEqual(self.queue.dequeue(), [1, 2, 3])
 
     def test_enqueue_dequeue_large_number(self):
         for i in range(100000):
-            self.q.enqueue(i)
-        self.assertEqual(self.q.len(), 100000)
-        self.assertEqual(self.q.dequeue(), 0)
-        self.assertEqual(self.q.len(), 99999)
-        
-        while not self.q.is_empty():
-            self.q.dequeue()
+            self.queue.enqueue(i)
+        self.assertEqual(self.queue.len(), 100000)
+        self.assertEqual(self.queue.dequeue(), 0)
+        self.assertEqual(self.queue.len(), 99999)
+
+        while not self.queue.is_empty():
+            self.queue.dequeue()
         self.assertIsNone(self.queue.dequeue())
+
 
 class LinkedListQueueTests(QueueTests):
     def setUp(self):
         self.queue = LinkedListQueue()
-    
+        
 if __name__ == "__main__":
     unittest.main()

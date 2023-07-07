e033d64f4f2e76b68270dd12c22658594b6f9ced
Giovani <giovanibulian@hotmail.com>
2023-07-07 16:29:05 -0400

    + sort method + tests

--- elementary_sorts/mergesort.py
+++ elementary_sorts/mergesort.py
@@ -3,14 +3,35 @@ class MergeSort:
     Given an array `a`, these are the basic steps:
 
     1) Divide array in two halves:
-            - left half:  `a[lo:mid]`
-            - right half: `a[mid:hi]`
+            - left half:  `a[lo, mid)`
+            - right half: `a[mid, hi)`
 
     2) Recursively sort each half
     3) Merge the two halves
     """
 
-    def merge(self, a: list, lo, mid, hi):
+    def sort(self, a):
+        n = len(a)
+        self._sort(a, lo=0, hi=n)
+
+    def _sort(self, a, lo, hi) -> None:
+        # 1) Base case: If the subarray size is 0 or 1,
+        # it is already sorted.
+        if (hi - lo) <= 1:
+            return
+
+        # 2) Recursive step:
+        #   2.1) compute mid = (hi + lo)//2
+        mid = (lo + hi) // 2
+
+        #   2.2) sort (recursively) the two subarrays
+        self._sort(a, lo=lo, hi=mid)  # a[lo:mid)
+        self._sort(a, lo=mid, hi=hi)  # a[mid:hi)
+
+        #   3.1) merge them.
+        self.merge(a, lo, mid, hi)
+
+    def merge(self, a, lo, mid, hi):
         """
         Abstract in-place merge: merge two sorted subarrays into a sorted array.
 
@@ -22,25 +43,28 @@ class MergeSort:
         - If any of the subarrays are exhausted, populate the remaining positions
         of `a` with the remaining items of the remaining subarray
         """
-
-        # check if the array is already sorted
-        if a[mid-1] <= a[mid]:
+        # this check is redundant, since it is already checked in the `_sort` method.
+        if (hi - lo) <= 1:
             return
 
         # assert that both subarrays are already sorted
         assert a[lo:mid] == sorted(a[lo:mid]), "Left subarray not sorted!"
         assert a[mid:hi] == sorted(a[mid:hi]), "Right subarray not sorted!"
-        
+
+        # IMPROVEMENT: check if the array is already sorted
+        if a[mid - 1] <= a[mid]:
+            return
+
         # auxiliary array
         aux = a.copy()
         left, right = lo, mid
 
-        for merged_index in range(len(a)):
+        for merged_index in range(lo, hi):
             # if any subarray is exhausted
             if left == mid or right == hi:
                 # populate the remaining positions with
                 # the remaining subarray's remaining items
-                a[merged_index:] = aux[left:mid] or aux[right:hi]
+                a[merged_index:hi] = aux[left:mid] or aux[right:hi]
                 break
 
             if aux[left] <= aux[right]:
--- elementary_sorts/tests_mergesort.py
+++ elementary_sorts/tests_mergesort.py
@@ -1,29 +1,66 @@
 from unittest import TestCase
 from mergesort import MergeSort
+from random import shuffle
 
 class TestMergeSort(TestCase):
     def setUp(self) -> None:
         self.mergesort = MergeSort()
     
+    def test_mergesort(self):
+        for n in range(1_000):
+            shuffle(a := list(range(n)))
+            self.mergesort.sort(a)
+            self.assertEqual(a, sorted(a))
+            
+    def test_sort_empty_array(self):
+        a = []
+        self.mergesort.sort(a)
+        self.assertEqual(a, [])
+
+    def test_sort_single_element(self):
+        a = [5]
+        self.mergesort.sort(a)
+        self.assertEqual(a, [5])
+
+    def test_sort_sorted_array(self):
+        a = [1, 2, 3, 4, 5]
+        self.mergesort.sort(a)
+        self.assertEqual(a, [1, 2, 3, 4, 5])
+
+    def test_sort_reverse_sorted_array(self):
+        a = [5, 4, 3, 2, 1]
+        self.mergesort.sort(a)
+        self.assertEqual(a, [1, 2, 3, 4, 5])
+
+    def test_sort_random_array(self):
+        a = [9, 2, 7, 1, 5, 3, 8, 4, 6]
+        self.mergesort.sort(a)
+        self.assertEqual(a, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_sort_duplicate_values(self):
+        a = [5, 2, 7, 1, 5, 3, 8, 4, 5]
+        self.mergesort.sort(a)
+        self.assertEqual(a, [1, 2, 3, 4, 5, 5, 5, 7, 8])
     
-    def test_unsorted_subarrays(self):
+
+class TestMerge(TestCase):
+    def setUp(self) -> None:
+        self.mergesort = MergeSort()
+    
+    def test_merge_unsorted_subarrays(self):
         
-        # unsorted left
+        # left subarray unsorted
         a = [1, 0, 2, 3, 4, 5]
         n = len(a)
-        lo = 0
-        mid = n//2
-        hi = n
+        lo, mid, hi = 0, n//2, n
         
         with self.assertRaises(AssertionError):
             self.mergesort.merge(a, lo, mid, hi)
             
-        # unsorted right
+        # right subarray unsorted
         a = [0, 1, 2, 3, 5, 4]
         n = len(a)
-        lo = 0
-        mid = n//2
-        hi = n
+        lo, mid, hi = 0, n//2, n
         
         with self.assertRaises(AssertionError):
             self.mergesort.merge(a, lo, mid, hi)
@@ -31,9 +68,7 @@ class TestMergeSort(TestCase):
         # both unsorted
         a = [0, 2, 1, 3, 5, 4]
         n = len(a)
-        lo = 0
-        mid = n//2
-        hi = n
+        lo, mid, hi = 0, n//2, n
         
         with self.assertRaises(AssertionError):
             self.mergesort.merge(a, lo, mid, hi)
@@ -54,28 +89,16 @@ class TestMergeSort(TestCase):
     
     
     def test_merge(self):
-        a = [
-            3, 5, 6,    # sorted left subarray
-            0, 1, 2,    # sorted right subarray
-        ]
-        
-        n = len(a)
-        lo = 0
-        mid = n//2
-        hi = n
-        
-        self.mergesort.merge(a, lo, mid, hi)
-        self.assertEqual(a, sorted(a))
-        
-        a = [
-            0, 1, 2,    # sorted left subarray
-            7, 8, 9,    # sorted right subarray
-        ]
-        
-        n = len(a)
-        lo = 0
-        mid = n//2
-        hi = n
-        
-        self.mergesort.merge(a, lo, mid, hi)
-        self.assertEqual(a, sorted(a))
+        for n in range(1_000):
+            shuffle(a:=list(range(n)))
+            n = len(a)
+            lo = 0
+            mid = n//2
+            hi = n
+            
+            # sort the subarrays
+            a[lo:mid] = sorted(a[lo:mid])            
+            a[mid:hi] = sorted(a[mid:hi])   
+                     
+            self.mergesort.merge(a, lo, mid, hi)
+            self.assertEqual(a, sorted(a))

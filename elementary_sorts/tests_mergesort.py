d6d3eee4a954c7b93960c32d572612223ec216e7
Giovani <giovanibulian@hotmail.com>
2023-07-07 11:08:45 -0400

    + edge cases

--- elementary_sorts/tests_mergesort.py
+++ elementary_sorts/tests_mergesort.py
@@ -3,7 +3,22 @@ from mergesort import MergeSort
 
 class TestMergeSort(TestCase):
     def setUp(self) -> None:
-        self.sorter = MergeSort()
+        self.mergesort = MergeSort()
+    
+    def test_merge_edge_cases(self):
+        
+        a = [] # empty list
+        self.mergesort.merge(a, 0, 0, 0)
+        self.assertEqual(a, [])
+
+        b = [5] # single element
+        self.mergesort.merge(b, 0, 1, 1)
+        self.assertEqual(b, [5])
+
+        c = [1, 2, 3, 4, 5] # already sorted list
+        self.mergesort.merge(c, 0, 3, 5)
+        self.assertEqual(c, [1, 2, 3, 4, 5])
+    
     
     def test_merge(self):
         a = [
@@ -16,6 +31,18 @@ class TestMergeSort(TestCase):
         mid = n//2
         hi = n
         
-        self.sorter.merge(a, lo, mid, hi)
+        self.mergesort.merge(a, lo, mid, hi)
         self.assertEqual(a, sorted(a))
         
+        a = [
+            0, 1, 2,    # sorted left subarray
+            7, 8, 9,    # sorted right subarray
+        ]
+        
+        n = len(a)
+        lo = 0
+        mid = n//2
+        hi = n
+        
+        self.mergesort.merge(a, lo, mid, hi)
+        self.assertEqual(a, sorted(a))

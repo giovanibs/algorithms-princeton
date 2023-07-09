bfa29c0a40c444baba3823fe7d4821142e7e74ca
Giovani <giovanibulian@hotmail.com>
2023-07-09 16:14:51 -0400

    + tests for doubling range

--- elementary_sorts/bottom_up_merge.py
+++ elementary_sorts/bottom_up_merge.py
@@ -9,8 +9,7 @@ class BottomUpMerge(MergeSort):
     def sort(self, a):
         n = len(a)
         
-        if n <= 1:
-            # array is sorted
+        if n <= 1: # array is sorted
             return
         
         for size in self.doubling_range(1, n):
@@ -23,10 +22,15 @@ class BottomUpMerge(MergeSort):
     
     def _sort(self):
         pass
-    
+
     def doubling_range(self, start, stop):
+        if start == 0:
+            raise ValueError("Start should be > 0")
+        if stop < start:
+            raise ValueError("Stop should be >= start")
+        
         i = start
-        while i < stop:
+        while i <= stop:
             yield i
             i *= 2
 
@@ -56,6 +60,50 @@ class TestsBottomUpMerge(TestMergeSort):
         a = [2, 1, 0]
         self.mergesort.sort(a)
         self.assertEqual(a, [0, 1, 2])
+        
+    def test_doubling_range_value_error(self):
+        start = 0
+        stop = 1
+        with self.assertRaises(ValueError):
+            list(self.mergesort.doubling_range(start, stop))
+        
+        start = 2
+        stop = 1
+        with self.assertRaises(ValueError):
+            list(self.mergesort.doubling_range(start, stop))
+        
+    def test_doubling_range(self):
+        start = 1
+        stop = 2
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [1, 2])
+        
+        start = 2
+        stop = 2
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [2])
+        
+        start = 1
+        stop = 3
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [1, 2])
+        
+        start = 1
+        stop = 4
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [1, 2, 4])
+        
+        start = 1
+        stop = 7
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [1, 2, 4])
+        
+        start = 2
+        stop = 7
+        doubling_list = list(self.mergesort.doubling_range(start, stop))
+        self.assertEqual(doubling_list, [2, 4])
+     
+    
         
 if __name__ == "__main__":
     unittest.main()

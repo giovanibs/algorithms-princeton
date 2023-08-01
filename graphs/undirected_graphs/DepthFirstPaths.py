7d41c0a60eaaf49b874ab97a31a9ff757ff02f36
Giovani <giovanibulian@hotmail.com>
2023-08-01 16:52:12 -0400

    + tests for _edge_to

--- graphs/undirected_graphs/DepthFirstPaths.py
+++ graphs/undirected_graphs/DepthFirstPaths.py
@@ -13,7 +13,7 @@ class DFP(DFS):
             raise TypeError(DFP.NOT_A_GRAPH)
         
         self._edge_to : list[int|None] = [None for _ in range(G.vertices_count)]
-        
+
         super().__init__(G, s)
 
     # redefine `_dfs` to populate `_edge_to`
@@ -35,3 +35,60 @@ class DFP(DFS):
 class TestsDFP(TestsDFS):
     def setUp(self) -> None:
         self.DFS = DFP   # test DFP against DFS tests
+        self.DFP = DFP
+
+    def test_100_dfs_no_edges(self):
+        V = 5
+        G = Graph(V)
+        
+        for v in range(G.vertices_count):
+            dfp = self.DFP(G, v)
+            self.assertFalse(any(dfp._edge_to))
+    
+    def test_101_two_connected_vertices(self):
+        V = 2
+        G = Graph(V)
+        v0, v1 = 0, 1
+        G.add_edge(v0, v1)
+        
+        dfp = self.DFP(G, v0)
+        self.assertIsNone(dfp._edge_to[v0])
+        self.assertEqual(dfp._edge_to[v1], v0)
+        
+        dfp = self.DFP(G, v1)
+        self.assertIsNone(dfp._edge_to[v1])
+        self.assertEqual(dfp._edge_to[v0], v1)
+        
+    def test_102_many_connected(self):
+        V = 4
+        G = Graph(V)
+        v0, v1, v2, v3 = 0, 1, 2, 3
+        G.add_edge(v0, v1)
+        G.add_edge(v1, v2)
+        G.add_edge(v1, v3)
+        
+        dfp = self.DFP(G, v0)
+        self.assertIsNone(dfp._edge_to[v0])
+        self.assertEqual(dfp._edge_to[v1], v0)
+        self.assertEqual(dfp._edge_to[v2], v1)
+        self.assertEqual(dfp._edge_to[v3], v1)
+        
+        dfp = self.DFP(G, v1)
+        self.assertIsNone(dfp._edge_to[v1])
+        self.assertEqual(dfp._edge_to[v0], v1)
+        self.assertEqual(dfp._edge_to[v2], v1)
+        self.assertEqual(dfp._edge_to[v3], v1)
+
+        dfp = self.DFP(G, v2)
+        self.assertIsNone(dfp._edge_to[v2])
+        self.assertEqual(dfp._edge_to[v0], v1)
+        self.assertEqual(dfp._edge_to[v1], v2)
+        self.assertEqual(dfp._edge_to[v3], v1)
+
+        dfp = self.DFP(G, v3)
+        self.assertIsNone(dfp._edge_to[v3])
+        self.assertEqual(dfp._edge_to[v0], v1)
+        self.assertEqual(dfp._edge_to[v1], v3)
+        self.assertEqual(dfp._edge_to[v2], v1)
+
+#

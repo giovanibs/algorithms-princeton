39e7850127f4dc83693137ac3f10703adb1a8a2d
Giovani <giovanibulian@hotmail.com>
2023-08-07 08:38:32 -0400

    MISC: renaming + comments editing

--- graphs/directed_graphs/DigraphDFS.py
+++ graphs/directed_graphs/DigraphDFS.py
@@ -8,13 +8,17 @@ class DigraphDFS:
     reached from a specified source vertex (or a
     group of source vertices) within a directed graph.
 
-    This approach employs a depth-first search technique.
+    It is the same method as for undirected graphs,
+    given that every undirected graph is a digraph
+    with edges in both directions.
+    
+    DFS is originally a digraph algorithm.
     """
     CONNECTED     = True
     NOT_A_DIGRAPH = "First argument must be a Digraph object."
 
     def __init__(self, DG: Digraph, source: int|set[int]) -> None:
-        # validation
+        # --- VALIDATION --- #
         if not isinstance(DG, Digraph):
             raise TypeError(DigraphDFS.NOT_A_DIGRAPH)
         
@@ -25,14 +29,14 @@ class DigraphDFS:
             if not DG.has_vertex(s):
                 raise IndexError(Digraph.VERTEX_NOT_IN_GRAPH)
         
-        # init
+        # --- INIT --- #
         self._S : int|set = source
         self._DG: Digraph = DG
 
         self._marked : list[bool]     = [ False for _ in range(DG.vertex_count)]
         self._edge_to: list[int|None] = [ None  for _ in range(DG.vertex_count)]
         
-        # search
+        # --- SEARCH --- #
         for s in source:
             if not self._marked[s]:
                 self._dfs(DG, s)
@@ -64,40 +68,42 @@ class DigraphDFS:
         return self._marked.count(DigraphDFS.CONNECTED)
     
 
+# ------------------------------------------------------------------------------
+# --- UNIT TESTS
+# ------------------------------------------------------------------------------
 import unittest
 
 class TestsDigraphDFS(unittest.TestCase):
     def setUp(self) -> None:
         V = 7
         self.G = Digraph(V)
-        (   self.source,
-            self.strictly_into,
-            self.strictly_out_of,
-            self.double_linked,
-            self.one_from_source,
-            self.two_from_source,
+        (   self.source             ,
+            self.strictly_inbound   ,
+            self.strictly_outbound  ,
+            self.double_linked      ,
+            self.one_from_source    ,
+            self.two_from_source    ,
             self.not_connected      ) = range(V) 
         
         self.not_in_graph = V
 
         # strictly in/outbound
-        self.G.add_edge( self.source         , self.strictly_into )
-        self.G.add_edge( self.strictly_out_of, self.source        )
-        
+        self.G.add_edge( self.source           , self.strictly_inbound )
+        self.G.add_edge( self.strictly_outbound, self.source           )
         # double link
         self.G.add_edge( self.source       , self.double_linked )
         self.G.add_edge( self.double_linked, self.source        )
-        
         # directed path
         self.G.add_edge( self.source         , self.one_from_source )
         self.G.add_edge( self.one_from_source, self.two_from_source )
 
         self.dfs = DigraphDFS(self.G, self.source)
 
+
     def test_000_init__type_error_not_a_digraph(self):
         with self.assertRaisesRegex(TypeError, DigraphDFS.NOT_A_DIGRAPH):
             DigraphDFS("G", 0)
-    
+
     def test_001_init__type_error_not_a_integer(self):
         with self.assertRaisesRegex(TypeError, Digraph.VERTEX_NOT_INTEGER):
             DigraphDFS(self.G, "0")
@@ -139,17 +145,17 @@ class TestsDigraphDFS(unittest.TestCase):
             self.assertEqual( len(dfs._edge_to), V      )
     
     def test_006_init__multisource(self):
-        multisource = {self.not_connected, self.strictly_out_of}
+        multisource = {self.not_connected, self.strictly_outbound}
 
-        multisource_dfs = DigraphDFS(self.G, multisource)
-        self.assertEqual( multisource_dfs._S, multisource )
+        dfs = DigraphDFS(self.G, multisource)
+        self.assertEqual( dfs._S, multisource )
 
-        self.assertTrue( multisource_dfs._marked[ self.one_from_source ] )
-        self.assertTrue( multisource_dfs._marked[ self.two_from_source ] )
-        self.assertTrue( multisource_dfs._marked[ self.strictly_into   ] )
-        self.assertTrue( multisource_dfs._marked[ self.double_linked   ] )
-        self.assertTrue( multisource_dfs._marked[ self.strictly_out_of ] )
-        self.assertTrue( multisource_dfs._marked[ self.not_connected   ] )
+        self.assertTrue( dfs._marked[ self.one_from_source  ] )
+        self.assertTrue( dfs._marked[ self.two_from_source  ] )
+        self.assertTrue( dfs._marked[ self.strictly_inbound ] )
+        self.assertTrue( dfs._marked[ self.double_linked    ] )
+        self.assertTrue( dfs._marked[ self.strictly_outbound] )
+        self.assertTrue( dfs._marked[ self.not_connected    ] )
     
     def test_100_dfs__graph_with_no_edges(self):
         V = 5
@@ -160,25 +166,25 @@ class TestsDigraphDFS(unittest.TestCase):
             self.assertEqual(dfs.count, 1) # only the vertex itself
     
     def test_101_dfs(self):
-        self.assertTrue ( self.dfs._marked[ self.one_from_source ] )
-        self.assertTrue ( self.dfs._marked[ self.two_from_source ] )
-        self.assertTrue ( self.dfs._marked[ self.strictly_into   ] )
-        self.assertTrue ( self.dfs._marked[ self.double_linked   ] )
-        self.assertFalse( self.dfs._marked[ self.strictly_out_of ] )
-        self.assertFalse( self.dfs._marked[ self.not_connected   ] )
+        self.assertTrue ( self.dfs._marked[ self.one_from_source  ] )
+        self.assertTrue ( self.dfs._marked[ self.two_from_source  ] )
+        self.assertTrue ( self.dfs._marked[ self.strictly_inbound ] )
+        self.assertTrue ( self.dfs._marked[ self.double_linked    ] )
+        self.assertFalse( self.dfs._marked[ self.strictly_outbound] )
+        self.assertFalse( self.dfs._marked[ self.not_connected    ] )
         
     def test_102_dfs__source_is_strictly_inbound_vertex(self):
-        self.assertTrue     ( self.dfs._marked [self.strictly_into] )
-        self.assertIsNotNone( self.dfs._edge_to[self.strictly_into] )
+        self.assertTrue     ( self.dfs._marked [self.strictly_inbound] )
+        self.assertIsNotNone( self.dfs._edge_to[self.strictly_inbound] )
 
-        dfs = DigraphDFS(self.G, self.strictly_into)
+        dfs = DigraphDFS(self.G, self.strictly_inbound)
         self.assertTrue( any(dfs._marked[1:]) )         # except itself 
 
     def test_103_dfs__source_is_strictly_outbound_vertex(self):
-        self.assertFalse ( self.dfs._marked [self.strictly_out_of] )
-        self.assertIsNone( self.dfs._edge_to[self.strictly_out_of] )
+        self.assertFalse ( self.dfs._marked [self.strictly_outbound] )
+        self.assertIsNone( self.dfs._edge_to[self.strictly_outbound] )
 
-        dfs = DigraphDFS(self.G, self.strictly_out_of)
+        dfs = DigraphDFS(self.G, self.strictly_outbound)
         self.assertTrue( all(dfs._marked[:-1]) )     # all except not_connected
         
     def test_104_dfs__source_is_double_linked(self):
@@ -195,7 +201,7 @@ class TestsDigraphDFS(unittest.TestCase):
         one_from_source = self.one_from_source
         
         dfs = DigraphDFS(self.G, one_from_source)
-        self.assertFalse( dfs._marked[old_source]           )
+        self.assertFalse( dfs._marked[old_source] )
         self.assertTrue ( dfs._marked[two_from_source] )
         
         dfs = DigraphDFS(self.G, two_from_source)
@@ -207,9 +213,9 @@ class TestsDigraphDFS(unittest.TestCase):
             self.dfs.is_reachable(self.not_in_graph)
     
     def test_201_is_reachable(self):
-        self.assertTrue ( self.dfs.is_reachable( self.one_from_source ) )
-        self.assertTrue ( self.dfs.is_reachable( self.two_from_source ) )
-        self.assertTrue ( self.dfs.is_reachable( self.strictly_into   ) )
-        self.assertTrue ( self.dfs.is_reachable( self.double_linked   ) )
-        self.assertFalse( self.dfs.is_reachable( self.strictly_out_of ) )
-        self.assertFalse( self.dfs.is_reachable( self.not_connected   ) )
+        self.assertTrue ( self.dfs.is_reachable( self.one_from_source  ) )
+        self.assertTrue ( self.dfs.is_reachable( self.two_from_source  ) )
+        self.assertTrue ( self.dfs.is_reachable( self.strictly_inbound ) )
+        self.assertTrue ( self.dfs.is_reachable( self.double_linked    ) )
+        self.assertFalse( self.dfs.is_reachable( self.strictly_outbound) )
+        self.assertFalse( self.dfs.is_reachable( self.not_connected    ) )
